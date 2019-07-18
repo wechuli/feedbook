7f01152430bc0ae126e03ae48e6f58f4c0f42788
Wechuli <wechulipaul@yahoo.com>
2019-07-18 12:24:53 +0300

    boiler plate for the app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
+//importing the node modules
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 
-
+//Instantiate the app
 const app = express();
+
+
+// Add necessary middleware
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+
 
 
 
