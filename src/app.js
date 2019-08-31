6f0ad20aafe1758aa64830187f27089e5db1fd47
Wechuli <wechulipaul@yahoo.com>
2019-08-31 20:36:58 +0300

    adding googla outh passport strategy and tests files setup

--- src/app.js
+++ src/app.js
@@ -1,46 +1,45 @@
 //importing the node modules
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 //Instantiate the app
 const app = express();
 
-
 // Add necessary middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: false
-}))
+  })
+);
 
 // Connect to MongoDB database
 
-
-
-
 // Custom Route Handlers
-
-
 
 //Default 404 page
 
 app.use((req, res) => {
-    res.status(404).json({
-        error: true,
-        message: "Route not found"
-    })
-})
+  res.status(404).json({
+    error: true,
+    message: "Route not found"
+  });
+});
 
+// passport oauth
+passport.use(new GoogleStrategy())
 
 // Get the port set in the environment
 const PORT = process.env.PORT || 8090;
 
-
 // listen on the specified port
 app.listen(PORT, () => {
-    console.info(`Server running on port ${PORT}`);
-})
+  console.info(`Server running on port ${PORT}`);
+});
