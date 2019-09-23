50f78b619480c9e024ae15027cab3c6f4127d1f8
Wechuli <wechulipaul@yahoo.com>
2019-09-23 16:07:18 +0300

    fix port issues with heroku

--- src/app.js
+++ src/app.js
@@ -65,6 +65,6 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 8090;
 
 // listen on the specified port
-app.listen(8090, () => {
-  console.info(`Server running on port 8090`);
+app.listen(PORT, () => {
+  console.info(`Server running on port ${PORT}`);
 });
