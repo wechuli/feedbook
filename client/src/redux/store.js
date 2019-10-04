3948ced04febd163fb160855d62fa66466e548da
Wechuli <wechulipaul@yahoo.com>
2019-10-04 15:50:53 +0300

    more redux setup arrg

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./rootReducer";
 
 const middlewares = [logger];
 
 const store = createStore(
-  () => [],
+  rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
-
 
 export default store;
